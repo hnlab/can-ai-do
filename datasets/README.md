a052df887d21f1ce9913573b84f202746077485c
jcyang <gold.yang@qq.com>
2019-01-14 18:43:16 +0800

    add DUD-E datasets

---
+++ datasets/README.md
@@ -0,0 +1,6 @@
+# [DUD-E dataset](http://dude.docking.org/)
+```bash
+bash download_dude.sh
+```
+
+
---
+++ datasets/download_dude.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget http://dude.docking.org/db/subsets/all/all.tar.gz
+tar -xvf all.tar.gz
+mv all dude

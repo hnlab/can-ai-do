525aaa7641118249024acff2a9d59c6296535ef8
jcyang <gold.yang@qq.com>
2020-11-30 13:18:27 +0800

    refine DUD-E download script.

--- tutorial/RF_DUD-E/1download_dude.sh
+++ tutorial/RF_DUD-E/1download_dude.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 wget http://dude.docking.org/db/subsets/all/all.tar.gz
 tar -xvf all.tar.gz
+
 # missing fgfr1 decoys in all.tar.gz
-cd all/fgfr1
-wget wget http://dude.docking.org/targets/fgfr1/decoys_final.mol2.gz
+cd all
+wget http://dude.docking.org/targets/fgfr1/fgfr1.tar.gz
+tar -xf fgfr1.tar.gz
+
+cd fgfr1/
+rm crystal_ligand.mol2
+wget http://dude.docking.org/targets/fgfr1/docking/xtal-lig.mol2 -O crystal_ligand.mol2
+rm receptor.pdb
+wget http://dude.docking.org/targets/fgfr1/docking/grids/rec.crg -O receptor.pdb
